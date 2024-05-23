2a103052ea0a646518edbbf9b95ff66b869bc880
webdiggers <webdiggers07@gmail.com>
2024-05-23 16:12:22 +0530

    fix: slot issue

--- resources/js/pages/admin/dashboard/DashboardPage.vue
+++ resources/js/pages/admin/dashboard/DashboardPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="d-flex flex-grow-1 flex-column">
     <h1>Welcome to {{ companyname }}</h1>
+    {{ bookings }}
   </div>
 </template>
 
@@ -19,92 +20,16 @@ export default {
     return {
       loadingInterval: null,
       companyname: JSON.parse(localStorage.getItem('store')).name,
-      isLoadingOverview: true,
-      isLoadingTransactions: true,
-      isLoadingAppointments: true,
       role: 0,
-      totalToday: 0,
-      totalThisMonth: 0,
-      totalThisYear: 0,
-      totalAllTime: 0,
-      todayAppointments: [],
-      transactions: [],
-      appointmrntsHeaders: [{
-        text: 'Patient Name',
-        align: 'start',
-        sortable: false,
-        value: 'patient.name'
-      },
-      {
-        text: 'Doctor Name',
-        value: 'doctor.name'
-      },
-      {
-        text: 'Booking',
-        value: 'booking_date'
-      },
-      {
-        text: 'Payment',
-        value: 'invoice.payment.status'
-      },
-      {
-        text: 'Status',
-        value: 'status'
-      },
-      {
-        text: 'Action',
-        value: 'action',
-        sortable: false
-      }
-      ],
-      trabsactionsHeader: [{
-        text: 'Pateint',
-        align: 'start',
-        value: 'patient.name',
-        sortable: false
-      }, {
-        text: 'Payment ID',
-        value: 'payment_id',
-      },
-      {
-        text: 'Patient Phone Number',
-        value: 'patient_phonenumber'
-      },
-      {
-        text: 'Order ID',
-        value: 'order_id'
-      },
-      {
-        text: 'Amount',
-        value: 'amount'
-      },
-      {
-        text: 'Currency',
-        value: 'currency'
-      },
-      {
-        text: 'Status',
-        value: 'status'
-      },
-      {
-        text: 'Method',
-        value: 'method'
-      },
-      {
-        text: 'Payment Date',
-        value: 'created_at'
-      },
+      bookings: []
 
-      ]
     }
   },
   beforeMount() {
     this.getAdminRoles();
   },
   mounted() {
-    // this.getBillingOverview();
-    // this.gettodayAppointments();
-    // this.getTransactions();
+    this.getBookings();
 
   },
   methods: {
@@ -121,64 +46,17 @@ export default {
           console.log(error)
         })
     },
-    getBillingOverview() {
-      axios.get('/api/admin/getpaymentsoverview')
+
+    async getBookings() {
+      await axios.post('/api/admin/bookings/list')
         .then(response => {
-          this.isLoading1 = false
-          if (response.data.success) {
-            this.totalAllTime = response.data.data.allpaymentsAmount;
-            this.totalThisYear = response.data.data.thisyearAmount;
-            this.totalThisMonth = response.data.data.thismonthAmount;
-            this.totalToday = response.data.data.todayAmount;
-          }
+          this.bookings = response.data.data;
+          console.log(response.data.data);
         })
         .catch(error => {
           console.log(error)
         })
-      this.isLoadingOverview = false;
-    },
-    gettodayAppointments() {
-      axios.get('/api/admin/gettodaywaittingappointments')
-        .then(response => {
-          this.isLoading1 = false
-          if (response.data.success) {
-            this.todayAppointments = response.data.records.data;
-          }
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      this.isLoadingAppointments = false;
-    },
-    getTransactions() {
-      axios.get('/api/admin/getpayments?limit=10')
-        .then(response => {
-          this.isLoading1 = false
-          if (response.data.success) {
-            this.transactions = response.data.data;
-          }
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      this.isLoadingTransactions = false;
-    },
-    editPatient($id) {
-      this.$router.push({
-        name: 'admin-edit-patient',
-        params: {
-          id: $id
-        }
-      })
-    },
-    editAppointment($id) {
-      this.$router.push({
-        name: 'admin-appointment-edit',
-        params: {
-          id: $id
-        }
-      })
-    },
+    }
 
   }
 }
